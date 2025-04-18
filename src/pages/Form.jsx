1823d108dcd824fa13dbd71a4b2c3bf2d49005c7
tbohn2 <tanner.bohn@yahoo.com>
2025-04-18 13:37:25 -0700

    Created form with update function

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react";
+
 function Form() {
+    const [formState, setFormState] = useState({
+        firstName: '',
+        lastName: '',
+        city: '',
+        state: '',
+        distanceWillingToTravel: 0
+    });
+
+    const updateForm = (e) => {
+        let { name, value } = e.target;
+        value = name === 'distanceWillingToTravel' ? parseInt(value) : value;
+
+        setFormState((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    useEffect(() => {
+        console.log(formState);
+    }, [formState])
 
     return (
         <div>
-            Form
+            <form>
+                <input type="text" name="firstName" value={formState.firstName} onChange={updateForm} required />
+                <input type="text" name="lastName" value={formState.lastName} onChange={updateForm} required />
+                <input type="text" name="city" value={formState.city} onChange={updateForm} required />
+                <input type="text" name="state" value={formState.state} onChange={updateForm} required />
+                <input type='number' name="distanceWillingToTravel" value={formState.distanceWillingToTravel} onChange={updateForm} min="0" step="5" required />
+            </form>
         </div>
     )
 }
