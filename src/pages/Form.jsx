c2c87fc44b455f38e8503a4754d4d0aa2e05838d
tbohn2 <tanner.bohn@yahoo.com>
2025-04-21 11:23:10 -0700

    Added validation to form

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -9,9 +9,11 @@ function Form() {
         distanceWillingToTravel: 0
     });
 
+    const [errors, setErrors] = useState({});
+
     const updateForm = (e) => {
         let { name, value } = e.target;
-        value = name === 'distanceWillingToTravel' ? parseInt(value) : value;
+        if (name === 'distanceWillingToTravel') { value = parseInt(value) };
 
         setFormState((prev) => ({
             ...prev,
@@ -19,18 +21,52 @@ function Form() {
         }));
     };
 
-    useEffect(() => {
-        console.log(formState);
-    }, [formState])
+    // useEffect(() => {
+    //     console.log(formState);
+    // }, [formState])
+
+    const validateForm = () => {
+        const newErrors = {};
+
+        const requiredTextFields = ['firstName', 'lastName', 'city', 'state']
+
+        requiredTextFields.forEach((field) => {
+            if (!formState[field]) {
+                const withSpaceAndCapitalized = field.replace(/([A-Z])/g, ' $1');
+                newErrors[field] = withSpaceAndCapitalized.charAt(0).toUpperCase() + withSpaceAndCapitalized.slice(1) + ' is required';
+            }
+        })
+
+        if (formState.distanceWillingToTravel === 0) { newErrors.distanceWillingToTravel = 'Distance must be positive' }
+
+        setErrors(newErrors);
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        setErrors({});
+        validateForm();
+    }
 
     return (
         <div>
-            <form>
-                <input type="text" name="firstName" value={formState.firstName} onChange={updateForm} required />
-                <input type="text" name="lastName" value={formState.lastName} onChange={updateForm} required />
-                <input type="text" name="city" value={formState.city} onChange={updateForm} required />
-                <input type="text" name="state" value={formState.state} onChange={updateForm} required />
-                <input type='number' name="distanceWillingToTravel" value={formState.distanceWillingToTravel} onChange={updateForm} min="0" step="5" required />
+            <form onSubmit={submitForm}>
+                {errors.firstName && <div>{errors.firstName}</div>}
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" name="firstName" value={formState.firstName} onChange={updateForm} />
+                {errors.lastName && <div>{errors.lastName}</div>}
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" name="lastName" value={formState.lastName} onChange={updateForm} />
+                {errors.city && <div>{errors.city}</div>}
+                <label htmlFor="city">City</label>
+                <input type="text" name="city" value={formState.city} onChange={updateForm} />
+                {errors.state && <div>{errors.state}</div>}
+                <label htmlFor="state">State</label>
+                <input type="text" name="state" value={formState.state} onChange={updateForm} />
+                {errors.distanceWillingToTravel && <div>{errors.distanceWillingToTravel}</div>}
+                <label htmlFor="distanceWillingToTravel">Distance willing to travel (Miles)</label>
+                <input type='number' name="distanceWillingToTravel" value={formState.distanceWillingToTravel} onChange={updateForm} min="0" step="5" />
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
