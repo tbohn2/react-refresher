2358855733e8a875a34d0f76a31110e11ee9fe51
tbohn2 <tanner.bohn@yahoo.com>
2025-04-21 18:06:30 -0700

    Styled and formated list

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import './list.css'
 
 function List() {
     const [scheduleItems, setScheduleItems] = useState([]);
@@ -10,8 +11,6 @@ function List() {
 
     useEffect(() => {
         const schedule = localStorage.getItem('schedule');
-
-
         if (schedule) { setScheduleItems(JSON.parse(schedule)) };
     }, [])
 
@@ -44,7 +43,6 @@ function List() {
         if (allItems.length > 1) {
             let i = allItems.length - 1;
             while (i > 0 && allItems[i].date < allItems[i - 1].date && allItems.length > 1) {
-                console.log(allItems);
                 [allItems[i - 1], allItems[i]] = [allItems[i], allItems[i - 1]]
                 i--;
             }
@@ -57,16 +55,32 @@ function List() {
     return (
         <div className="list">
             <h1>Meeting Times</h1>
+            <div className="grid-container">
+                <div>
+                    Location
+                </div>
+                <div>
+                    Date
+                </div>
+            </div>
             {scheduleItems.length > 0 &&
                 scheduleItems.map((item, index) => {
                     return (
-                        <div key={index}>
+                        <div key={index} className="grid-container">
                             <div>
                                 {item.location}
                             </div>
                             <div>
-                                {item.date}
+                                {new Date(item.date).toLocaleString('en-US', {
+                                    weekday: 'long',
+                                    year: 'numeric',
+                                    month: 'short',
+                                    day: '2-digit',
+                                    hour: '2-digit',
+                                    minute: '2-digit'
+                                })}
                             </div>
+                            <button className="danger">Delete</button>
                         </div>
                     )
                 })}
@@ -77,11 +91,11 @@ function List() {
                     <input type="text" name='location' onChange={handleChange} />
                     <label htmlFor="date"></label>
                     <input type='datetime-local' name='date' onChange={handleChange} />
-                    <button onClick={handleAddItem}>Add Item</button>
+                    <button className="success" onClick={handleAddItem}>Add Item</button>
                 </div>
             }
 
-            <button onClick={toggleAdding}>{adding ? 'Cancel' : 'Add to Schedule'}</button>
+            <button className="success" onClick={toggleAdding}>{adding ? 'Cancel' : 'Add to Schedule'}</button>
         </div>
     )
 }
